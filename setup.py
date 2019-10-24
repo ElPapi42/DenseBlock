1fe2dbee1495feea7cf1b35ee910641b3233765f
ElPapi42 <whitman-2@hotmail.com>
2019-10-24 15:35:20 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
 	],
 	classifiers=[
 		"Programming Language :: Python :: 3",
-		"License :: MIT License",
+		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
 	python_requires='>=3.7',
